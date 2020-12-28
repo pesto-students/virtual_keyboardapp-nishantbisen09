4c9f8e9d0c8de90e403f7f81a1b7376dc6e04e6c
nishantbisen09 <nishantbisen09@gmail.com>
2020-12-28 19:01:25 +0530

    Virtual Keyboard | Implementation

---
+++ src/constants/appConstants.js
@@ -0,0 +1,237 @@
+const KEYS = {
+  firstRow: [
+    {
+      label: "`",
+      shiftLabel: "~",
+    },
+    {
+      label: "1",
+      shiftLabel: "!",
+    },
+    {
+      label: "2",
+      shiftLabel: "@",
+    },
+    {
+      label: "3",
+      shiftLabel: "#",
+    },
+    {
+      label: "4",
+      shiftLabel: "$",
+    },
+    {
+      label: "5",
+      shiftLabel: "%",
+    },
+    {
+      label: "6",
+      shiftLabel: "^",
+    },
+    {
+      label: "7",
+      shiftLabel: "&",
+    },
+    {
+      label: "8",
+      shiftLabel: "*",
+    },
+    {
+      label: "9",
+      shiftLabel: "(",
+    },
+    {
+      label: "0",
+      shiftLabel: ")",
+    },
+    {
+      label: "-",
+      shiftLabel: "_",
+    },
+    {
+      label: "=",
+      shiftLabel: "+",
+    },
+  ],
+  secondRow: [
+    {
+      label: "q",
+      shiftLabel: "Q",
+    },
+    {
+      label: "w",
+      shiftLabel: "W",
+    },
+    {
+      label: "e",
+      shiftLabel: "E",
+    },
+    {
+      label: "r",
+      shiftLabel: "R",
+    },
+    {
+      label: "t",
+      shiftLabel: "T",
+    },
+    {
+      label: "y",
+      shiftLabel: "Y",
+    },
+    {
+      label: "u",
+      shiftLabel: "U",
+    },
+    {
+      label: "i",
+      shiftLabel: "I",
+    },
+    {
+      label: "o",
+      shiftLabel: "O",
+    },
+    {
+      label: "p",
+      shiftLabel: "P",
+    },
+    {
+      label: "[",
+      shiftLabel: "{",
+    },
+    {
+      label: "]",
+      shiftLabel: "}",
+    },
+    {
+      label: "\\",
+      shiftLabel: "|",
+    },
+  ],
+  thirdRow: [
+    {
+      label: "a",
+      shiftLabel: "A",
+    },
+    {
+      label: "s",
+      shiftLabel: "S",
+    },
+    {
+      label: "d",
+      shiftLabel: "D",
+    },
+    {
+      label: "f",
+      shiftLabel: "F",
+    },
+    {
+      label: "g",
+      shiftLabel: "G",
+    },
+    {
+      label: "h",
+      shiftLabel: "H",
+    },
+    {
+      label: "j",
+      shiftLabel: "J",
+    },
+    {
+      label: "k",
+      shiftLabel: "K",
+    },
+    {
+      label: "l",
+      shiftLabel: "L",
+    },
+    {
+      label: ";",
+      shiftLabel: ":",
+    },
+    {
+      label: "'",
+      shiftLabel: '"',
+    },
+    {
+      label: "Enter",
+      shiftLabel: "Enter",
+    },
+  ],
+  fourthRow: [
+    {
+      label: "Shift",
+      shiftLabel: "Shift",
+    },
+    {
+      label: "z",
+      shiftLabel: "Z",
+    },
+    {
+      label: "x",
+      shiftLabel: "X",
+    },
+    {
+      label: "c",
+      shiftLabel: "C",
+    },
+    {
+      label: "v",
+      shiftLabel: "V",
+    },
+    {
+      label: "b",
+      shiftLabel: "B",
+    },
+    {
+      label: "n",
+      shiftLabel: "N",
+    },
+    {
+      label: "m",
+      shiftLabel: "M",
+    },
+    {
+      label: ",",
+      shiftLabel: "<",
+    },
+    {
+      label: ".",
+      shiftLabel: ">",
+    },
+    {
+      label: "/",
+      shiftLabel: "?",
+    },
+    {
+      label: "Shift",
+      shiftLabel: "Shift",
+    },
+  ],
+  fifthRow: [
+    {
+      label: 'Bksp',
+      shiftLabel: "Bksp",
+    },
+    {
+      label: "Space",
+      shiftLabel: "Space",
+    },
+    {
+      label: "Clear",
+      shiftLabel: "Clear",
+    },
+    {
+      label: "Cancel",
+      shiftLabel: "Cancel",
+    },
+  ],
+};
+
+const ACTION_KEYS = {
+  Shift: 'Shift',
+  Clear: 'Clear',
+  Backspace: 'Bksp',
+  Enter: 'Enter',
+  Cancel:  'Cancel',
+  Space: 'Space'
+}
